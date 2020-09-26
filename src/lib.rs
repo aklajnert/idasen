c4fece6f571b76c3e3d0f6f5cb4cd0f8fbc1c33a
Andrzej Klajnert <github@aklajnert.pl>
2020-09-26 18:59:42 +0200

    Got up/down controls and reading raw height

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
 extern crate btleplug;
 
-const CONTROL: &str = "99fa0002338a10248a49009c0215f78a";
-
-use btleplug::api::{Central, CharPropFlags, Characteristic, Peripheral, UUID};
+use btleplug::api::{Central, Characteristic, Peripheral, UUID};
 #[cfg(target_os = "linux")]
 use btleplug::bluez::{adapter::ConnectedAdapter, manager::Manager};
 #[cfg(target_os = "macos")]
 use btleplug::corebluetooth::{adapter::Adapter, manager::Manager};
 #[cfg(target_os = "windows")]
 use btleplug::winrtble::{adapter::Adapter, manager::Manager};
+use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
 
@@ -28,7 +27,29 @@ fn get_central(manager: &Manager) -> ConnectedAdapter {
     adapter.connect().unwrap()
 }
 
-pub fn main() {
+const CONTROL_UUID: UUID = UUID::B128([
+    0x8a, 0xf7, 0x15, 0x02, 0x9c, 0x00, 0x49, 0x8a, 0x24, 0x10, 0x8a, 0x33, 0x02, 0x00, 0xfa, 0x99,
+]);
+
+const STATUS_UUID: UUID = UUID::B128([
+    0x8a, 0xf7, 0x15, 0x02, 0x9c, 0x00, 0x49, 0x8a, 0x24, 0x10, 0x8a, 0x33, 0x21, 0x00, 0xfa, 0x99,
+]);
+const POSITION_UUID: UUID = UUID::B128([
+    0x8a, 0xf7, 0x15, 0x02, 0x9c, 0x00, 0x49, 0x8a, 0x24, 0x10, 0x8a, 0x33, 0x20, 0x00, 0xfa, 0x99,
+]);
+
+const UP: [u8; 2] = [0x47, 0x00];
+const DOWN: [u8; 2] = [0x46, 0x00];
+const STOP: [u8; 2] = [0xFF, 0x00];
+
+const MIN_HEIGHT: f32 = 0.62;
+const MAX_HEIGHT: f32 = 1.27;
+
+fn bytes_to_meters(bytes: [u8; 4]) -> f32 {
+    0.0
+}
+
+pub fn main() -> Vec<Characteristic> {
     let manager = Manager::new().unwrap();
 
     // get the first bluetooth adapter
@@ -60,9 +81,26 @@ pub fn main() {
 
     // discover characteristics
     let characteristics = desk.discover_characteristics().unwrap();
-    for char in &characteristics {
-        println!("{:?}", char.uuid);
+    let control_characteristic = characteristics
+        .iter()
+        .find(|characteristic| characteristic.uuid == CONTROL_UUID)
+        .unwrap();
+    let status_characteristic = characteristics
+        .iter()
+        .find(|characteristics| characteristics.uuid == STATUS_UUID)
+        .unwrap();
+
+    println!("{:?}", desk.command(&control_characteristic, &UP));
+    println!("{:?}", desk.command(&control_characteristic, &STOP));
+    thread::sleep(Duration::from_secs(1));
+    println!("{:?}", desk.command(&control_characteristic, &DOWN));
+    println!("{:?}", desk.command(&control_characteristic, &STOP));
+
+    loop {
+        let response = desk.read_by_type(&status_characteristic, status_characteristic.uuid);
+        println!("H: {:?}", response);
+        thread::sleep(Duration::from_secs(1));
     }
 
-    println!("{:?}", desk.characteristics());
+    characteristics
 }
